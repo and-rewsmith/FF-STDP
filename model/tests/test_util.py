94c2e91a645c25a967eb2a1122ea9a8add80f125
Andrew <andrew.smith.recruiting@gmail.com>
2024-02-23 12:33:42 -0500

    fix tests

--- model/tests/test_util.py
+++ model/tests/test_util.py
@@ -8,14 +8,14 @@ def test_temporal_filter() -> None:
     tf = TemporalFilter(tau_rise=1, tau_fall=1)
 
     # Initial apply since temporal filter is second order
-    tf.apply(value=torch.Tensor([1]))
+    tf.apply(value=torch.Tensor([1]), dt=1)
 
     # Apply a single error with a small dt should result in small change
-    assert tf.apply(value=torch.Tensor([1])).item(
+    assert tf.apply(value=torch.Tensor([1]), dt=1).item(
     ) == pytest.approx(1.7357588823428847)
 
     # Apply a zero error should result in decay of the state
-    assert tf.apply(value=torch.Tensor([0])).item(
+    assert tf.apply(value=torch.Tensor([0]), dt=1).item(
     ) == pytest.approx(1.1417647320527227)
 
 
@@ -23,13 +23,13 @@ def test_spike_moving_average() -> None:
     sma = SpikeMovingAverage(batch_size=1, tau_mean=1, data_size=1)
 
     # Apply a single spike
-    assert sma.apply(spike=torch.Tensor([1])).item() == 1.0
+    assert sma.apply(spike=torch.Tensor([1]), dt=1).item() == 1.0
 
     # Apply another spike, the average should increase
-    assert sma.apply(spike=torch.Tensor([2])).item() == 2.0
+    assert sma.apply(spike=torch.Tensor([2]), dt=1).item() == 2.0
 
     # After some time with no spikes, the average should decay
-    assert sma.apply(spike=torch.Tensor([0])).item() == 0.0
+    assert sma.apply(spike=torch.Tensor([0]), dt=1).item() == 0.0
 
 
 def test_variance_moving_average() -> None:
@@ -42,24 +42,24 @@ def test_variance_moving_average() -> None:
     spike = torch.Tensor([2])
     for _ in range(20):
         # NOTE: The spike moving average is always updated first
-        sma.apply(spike=spike)
-        vma.apply(spike=spike, spike_moving_average=sma.tracked_value())
+        sma.apply(spike=spike, dt=1)
+        vma.apply(spike=spike, spike_moving_average=sma.tracked_value(), dt=1)
     assert sma.tracked_value().item() > 1
 
-    sma_value = sma.apply(spike=spike)
+    sma_value = sma.apply(spike=spike, dt=1)
     expected_variance = 0.3508073062162211
     assert vma.apply(
-        spike=spike, spike_moving_average=sma_value).item() == pytest.approx(expected_variance)
+        spike=spike, spike_moving_average=sma_value, dt=1).item() == pytest.approx(expected_variance)
 
     # After some time with no spikes, the variance should increase
     spike_2 = torch.Tensor([0])
-    sma_value = sma.apply(spike=spike_2)  # Mean should decay
+    sma_value = sma.apply(spike=spike_2, dt=1)  # Mean should decay
     assert vma.apply(
-        spike=spike_2, spike_moving_average=sma_value).item() > expected_variance
+        spike=spike_2, spike_moving_average=sma_value, dt=1).item() > expected_variance
 
     # Apply another spike, the variance should adjust based on the new mean
     spike_3 = torch.Tensor([5])
-    sma_value = sma.apply(spike=spike_3)  # Mean should increase
+    sma_value = sma.apply(spike=spike_3, dt=1)  # Mean should increase
     variance_before = vma.tracked_value().item()
     assert vma.apply(
-        spike=spike_3, spike_moving_average=sma_value).item() > variance_before
+        spike=spike_3, spike_moving_average=sma_value, dt=1).item() > variance_before
