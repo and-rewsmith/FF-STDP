cbcd9dd57d8b9bcb5e5afb2eaae30c5f45d10a5d
Andrew Smith <andrew.smith.recruiting@gmail.com>
2024-03-01 16:20:43 -0800

    [feat]: custom LIF neuron (#26)
    Adding a custom LIF neuron so that we don't need to depend on arbitrary
    implementation details of snnTorch. I have swapped out snnTorch in favor
    of this new custom LIF.

---
+++ model/src/lif.py
@@ -0,0 +1,39 @@
+from typing import Any, Optional
+
+import torch
+import torch.nn as nn
+
+
+class LIF(nn.Module):
+    def __init__(self, beta: float, threshold: float = 1.0):
+        """
+        NOTE: Initialization of mem to zeros at initial forward pass will cause
+        transient behavior
+
+        NOTE: The membrane potential is not resting at a negative value, as it
+        is in Zenke's work. This implementation may need to change.
+        """
+        super(LIF, self).__init__()
+        # Initialize decay rate beta and threshold
+        self.beta = beta
+        self.threshold = threshold
+        self.spike_op = self.SpikeOperator.apply
+        self.mem: Optional[torch.Tensor] = None
+
+    def forward(self, current: torch.Tensor) -> torch.Tensor:
+        if self.mem == None:  # noqa
+            self.mem = torch.zeros_like(current)
+
+        spk: torch.Tensor = self.spike_op(self.mem, self.threshold)
+        reset = spk * self.threshold
+        self.mem = self.beta * self.mem + current - reset  # type: ignore [operator]
+        return spk
+
+    class SpikeOperator(torch.autograd.Function):
+
+        @staticmethod
+        def forward(ctx: Any, mem: torch.Tensor, threshold: torch.Tensor) -> torch.Tensor:
+            input = mem - threshold
+            spk = torch.zeros_like(input)
+            spk[input > 0] = 1.0
+            return spk
