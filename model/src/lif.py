6dfb652f5cf8837600fdcbd151928c48a61e1860
Andrew <andrew.smith.recruiting@gmail.com>
2024-03-05 15:25:16 -0800

    [feat]: change LIF to issue spike at same timestep (#30)
    Changes:
    1. Change LIF to issue spike at same timestep
    2. Record prereset membrane potential. This change is made such that the
    learning rule and logging does not observe a < 1 membrane potential for
    a spike.
    3. Use the prereset membrane potential in the learning rule. This is not
    what Zenke did and I noted that in case we need to change later.

--- model/src/lif.py
+++ model/src/lif.py
@@ -12,6 +12,17 @@ class LIF(nn.Module):
 
         NOTE: The membrane potential is not resting at a negative value, as it
         is in Zenke's work. This implementation may need to change.
+
+        NOTE: The ordering of the following operations within a timestep is
+        important. So we need to pick the correct ordering. The current
+        implementation follows Zenke's work, HOWEVER, the membrane potential
+        used in the learning rule is separate from this implementation, and may
+        either use the `mem` or `prereset_mem`.
+
+        Order of operations:
+        1. Update membrane potential
+        2. Spike if membrane potential exceeds threshold
+        3. Reset the membrane potential if spiked
         """
         super(LIF, self).__init__()
         # Initialize decay rate beta and threshold
@@ -19,14 +30,25 @@ class LIF(nn.Module):
         self.threshold = threshold
         self.spike_op = self.SpikeOperator.apply
         self.mem: Optional[torch.Tensor] = None
+        self.prereset_mem: Optional[torch.Tensor] = None
 
     def forward(self, current: torch.Tensor) -> torch.Tensor:
         if self.mem == None:  # noqa
             self.mem = torch.zeros_like(current)
+            self.prereset_mem = torch.zeros_like(current)
 
+        # Update membrane potential: decay and add current
+        assert self.mem is not None
+        self.mem = self.beta * self.mem + current
+        self.prereset_mem = self.mem.clone()
+
+        # Spike if membrane potential exceeds threshold
         spk: torch.Tensor = self.spike_op(self.mem, self.threshold)
+
+        # Reset the membrane potential if spiked
         reset = spk * self.threshold
-        self.mem = self.beta * self.mem + current - reset  # type: ignore [operator]
+        self.mem -= reset  # type: ignore [operator]
+
         return spk
 
     class SpikeOperator(torch.autograd.Function):
--- model/src/util.py
+++ model/src/util.py
@@ -1,6 +1,6 @@
 import math
 from collections import deque
-from typing import Any, Deque, Optional, Tuple
+from typing import Deque, Optional
 
 import torch
 
@@ -42,10 +42,13 @@ class MovingAverageLIF():
         return (spike)
 
     def mem(self) -> torch.Tensor:
-        if self.neuron_layer.mem is None:
+        if self.neuron_layer.prereset_mem is None:
             raise ValueError("No data has been received yet")
 
-        return self.neuron_layer.mem
+        # NOTE: The membrane potential returned here is used in the learning
+        # rule. Currently, we are not returning what Zenke's paper uses (they
+        # use `neuron_layer.mem`).
+        return self.neuron_layer.prereset_mem
 
     def tracked_spike_moving_average(self) -> torch.Tensor:
         return self.spike_moving_average.tracked_value()
