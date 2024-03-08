1570b9ba08e520f978e72cc063a889d1567a4407
Andrew <andrew.smith.recruiting@gmail.com>
2024-03-08 15:39:56 -0500

    [ci]: fix ci (#38)
    Fixing CI script

--- scripts/devops/smoke.sh
+++ scripts/devops/smoke.sh
@@ -8,6 +8,6 @@ export WANDB_MODE=dryrun
 python -m model.tests.smoke.smoke_zenke_2a > ci_log.txt 2>&1 || python_exit_code=$?
 
 if [ -n "$python_exit_code" ]; then
-    cat log.txt
+    cat ci_log.txt
     exit $python_exit_code
 fi
