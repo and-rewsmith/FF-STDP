64d51a5285c3ca4bebe8b569a9fb1672f8a00429
Andrew <andrew.smith.recruiting@gmail.com>
2024-03-28 19:15:09 -0400

    prog

--- rl/benchmarks/src/memory_path_rl_sanity.py
+++ rl/benchmarks/src/memory_path_rl_sanity.py
@@ -7,25 +7,33 @@ import gymnasium as gym
 import wandb
 
 ACTION_DIM = 3
-ACTOR_LR = 1e-4
-CRITIC_LR = 1e-4
+ACTOR_LR = 1e-6 * 5
+CRITIC_LR = 1e-6 * 5
 
-EPISODES_SWITCH_AFTER = 300
+EPISODES_SWITCH_AFTER = 250
 
-HIDDEN_LAYER_SIZE = 1024
+HIDDEN_LAYER_SIZE = 2048
+LAST_LAYER_SIZE = 64
+
+TIMESTEPS = 80
+
+NUM_STORED_STATES = 10
 
 
 class Actor(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Actor, self).__init__()
         self.network = nn.Sequential(
+            # nn.Linear(input_dim, output_dim),
             nn.Linear(input_dim, HIDDEN_LAYER_SIZE),
             nn.ReLU(),
             nn.Linear(HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE // 2),
+            # nn.ReLU(),
+            # nn.Linear(HIDDEN_LAYER_SIZE // 2, output_dim),
             nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_SIZE // 2, HIDDEN_LAYER_SIZE // 4),
+            nn.Linear(HIDDEN_LAYER_SIZE // 2, LAST_LAYER_SIZE),
             nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_SIZE // 4, output_dim),
+            nn.Linear(LAST_LAYER_SIZE, output_dim),
             nn.Softmax(dim=-1)
         )
 
@@ -41,9 +49,10 @@ class Critic(nn.Module):
             nn.ReLU(),
             nn.Linear(HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE // 2),
             nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_SIZE // 2, HIDDEN_LAYER_SIZE // 4),
+            # nn.Linear(HIDDEN_LAYER_SIZE // 2, 1)
+            nn.Linear(HIDDEN_LAYER_SIZE // 2, LAST_LAYER_SIZE),
             nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_SIZE // 4, 1),
+            nn.Linear(LAST_LAYER_SIZE, 1),
         )
 
     def forward(self, state):
@@ -119,17 +128,18 @@ torch.manual_seed(1234)
 torch.set_printoptions(precision=10, sci_mode=False)
 
 # environment_seeds = [4, 383, 428, 410, 147, 63, 698, 911, 919, 135, 904, 109, 146, 1, 178, 16, 189, 337]
-environment_seeds = [146, 1, 919, 178, 4, 16, 904]
+# environment_seeds = [4, 146, 1, 919, 178, 4, 16, 904]
+environment_seeds = [146]
 env = gym.make('MiniGrid-FourRooms-v0',
                render_mode='human',
-               max_steps=80)
+               max_steps=TIMESTEPS)
 env.action_space.seed(42)
 state, _ = env.reset(seed=4)
 state_dim_1 = state["image"].shape[0]
 state_dim_2 = state["image"].shape[1]
 state_dim_3 = state["image"].shape[2]
-total_state_dim = 5 * (VISION_SIZE * VISION_SIZE *
-                       (NUM_OBJECTS + NUM_COLORS + NUM_STATES) + NUM_DIRECTIONS) + 1
+total_state_dim = NUM_STORED_STATES * (VISION_SIZE * VISION_SIZE *
+                                       (NUM_OBJECTS + NUM_COLORS + NUM_STATES) + NUM_DIRECTIONS) + TIMESTEPS
 
 actor = Actor(total_state_dim, ACTION_DIM)
 critic = Critic(total_state_dim)
@@ -137,7 +147,7 @@ critic = Critic(total_state_dim)
 actor_optim = optim.Adam(actor.parameters(), lr=ACTOR_LR)
 critic_optim = optim.Adam(critic.parameters(), lr=CRITIC_LR)
 
-num_episodes = 10000
+num_episodes = 100000
 gamma = 0.99
 
 successes = 0
@@ -145,15 +155,15 @@ failures = 0
 
 states = []
 for episode in range(num_episodes):
-    # if episode // EPISODES_SWITCH_AFTER >= len(environment_seeds):
-    #     environment_seed = random.randint(0, 1000)
-    #     print(f"Episode: {episode}, Seed: {environment_seed}")
-    # else:
-    #     environment_seed = environment_seeds[episode // EPISODES_SWITCH_AFTER]
+    if episode // EPISODES_SWITCH_AFTER >= len(environment_seeds):
+        environment_seed = random.randint(0, 1000)
+        print(f"Episode: {episode}, Seed: {environment_seed}")
+    else:
+        environment_seed = environment_seeds[episode // EPISODES_SWITCH_AFTER]
 
-    random_seed_from_seeds = random.randint(0, len(environment_seeds) - 1)
+    # random_seed_from_seeds = random.randint(0, len(environment_seeds) - 1)
     # environment_seed = environment_seeds[random_seed_from_seeds]
-    environment_seed = 4
+    # environment_seed = 4
 
     print(f"Episode: {episode}, Seed: {environment_seed}")
 
@@ -163,25 +173,42 @@ for episode in range(num_episodes):
     state = convert_observation_to_spike_input(vision, direction)
 
     states.append(state)
-    if len(states) > 5:
+    if len(states) > NUM_STORED_STATES:
         states.pop(0)
-    while len(states) < 5:
+    while len(states) < NUM_STORED_STATES:
         states.append(state)
 
     timestep = 0
     total_state_tensor = torch.cat(states, dim=1)
-    total_state_tensor = torch.cat((total_state_tensor, torch.Tensor([[timestep]])), dim=1)
+    timestep_tensor = torch.zeros(TIMESTEPS)
+    timestep_tensor[timestep-1] = 1
+    timestep_tensor = timestep_tensor.unsqueeze(0)
+    total_state_tensor = torch.cat((total_state_tensor, timestep_tensor), dim=1)
 
     done = False
     total_reward = 0
 
+    prev_actor_loss = None  # TODOPRE: remove
+    prev_prob_move = None  # TODOPRE: remove
+    old_state = None  # TODOPRE: remove
     while not done:
         total_state_tensor = torch.FloatTensor(total_state_tensor)
         probs = actor(total_state_tensor)
+        print(f"probs: {probs}")
+
         dist = torch.distributions.Categorical(probs)
         action = dist.sample()
+        # print(f"action: {action}")
 
-        next_single_state, reward, terminated, truncated, _ = env.step(action.item())
+        # # TODOPRE: remove
+        # # if probs is higher relative to old probs, and loss was neg, debug
+        # if prev_actor_loss is not None and prev_prob_move is not None:
+        #     # if torch.equal(total_state_tensor[:, 0:-2], old_state[:, 0:-2]) and prev_actor_loss < 0 and probs[0][action].item() - prev_prob_move < 0:
+        #     if torch.equal(total_state_tensor, old_state) and prev_actor_loss < 0 and probs[0][action].item() - prev_prob_move > 0:
+        #         input()
+
+        old_state = total_state_tensor  # TODOPRE: remove
+        observation, reward, terminated, truncated, _ = env.step(action.item())
 
         if terminated:
             successes += 1
@@ -189,26 +216,35 @@ for episode in range(num_episodes):
             failures += 1
 
         done = terminated or truncated
-        vision = next_single_state["image"]
-        direction = next_single_state["direction"]
+        vision = observation["image"]
+        direction = observation["direction"]
         next_single_state = convert_observation_to_spike_input(vision, direction)
         total_reward += reward
 
         states.append(next_single_state)
-        if len(states) > 5:
+        if len(states) > NUM_STORED_STATES:
             states.pop(0)
 
+        timestep += 1
         total_state_tensor_next = torch.cat(states, dim=1)
-        total_state_tensor_next = torch.cat((total_state_tensor_next, torch.Tensor([[timestep]])), dim=1)
+        timestep_tensor = torch.zeros(TIMESTEPS)
+        timestep_tensor[timestep-1] = 1
+        timestep_tensor = timestep_tensor.unsqueeze(0)
+        total_state_tensor_next = torch.cat((total_state_tensor_next, timestep_tensor), dim=1)
+        # total_state_tensor_next = torch.cat((total_state_tensor_next, torch.Tensor([[timestep]])), dim=1)
 
-        if episode < EPISODES_SWITCH_AFTER and not is_in_bounds(env.agent_pos):
-            done = True
+        # if episode < EPISODES_SWITCH_AFTER and not is_in_bounds(env.agent_pos):
+        #     done = True
 
         # Update Critic
         value = critic(total_state_tensor)
         wandb.log({"value": value})
         next_value = critic(torch.FloatTensor(total_state_tensor_next))
         td_error = reward + (gamma * next_value * (1 - int(done))) - value
+        # print(f"reward: {reward}")
+        # print(f"next_value: {next_value}")
+        # print(f"value: {value}")
+        # print(f"td_error: {td_error}")
         wandb.log({"td_error": td_error})
 
         critic_loss = td_error ** 2
@@ -219,7 +255,10 @@ for episode in range(num_episodes):
 
         # Update Actor
         actor_loss = -dist.log_prob(action) * td_error.detach()
+        prev_actor_loss = actor_loss.item()
         wandb.log({"actor_loss": actor_loss})
+        # print(f"actor_loss: {actor_loss}")
+        # print()
         actor_optim.zero_grad()
         actor_loss.backward()
         actor_optim.step()
@@ -229,6 +268,8 @@ for episode in range(num_episodes):
         wandb.log({"successes": successes})
         wandb.log({"failures": failures})
 
-        timestep += 1
+        # TODOPRE: remove
+        prev_prob_move = probs[0][action].item()
+        prev_actor_loss = actor_loss.item()
 
     print(f'Episode: {episode}, Total Reward: {total_reward}')
