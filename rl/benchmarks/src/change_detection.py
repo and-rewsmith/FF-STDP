4d1a65537689d05508bf1b880bc6669d2dc89052
Andrew <andrew.smith.recruiting@gmail.com>
2024-04-19 09:47:27 -0700

    working but slow

--- rl/benchmarks/src/change_detection.py
+++ rl/benchmarks/src/change_detection.py
@@ -1,19 +1,24 @@
-import gymnasium as gym
 import random
+
+import gymnasium as gym
 from torch import nn
+from torch import optim
+import torch
+import wandb
+from model.src.network import Net
+from model.src.settings import Settings
+from model.src.visualizer import NetworkVisualizer
 
 from rl.benchmarks.src.change_detection_framework import ChangeDetectionBasic
 
 """
 TODO: 
 implement actor critic
-assess lpl decodability on trained policy (train policy helps decoding the reward)
 implement the bridge from state to the LPL network, and from LPL network to actor / critic
 batch processing
 if that doesn't work, implement some sort of search
 
 verify metaplasticity
-
 """
 
 ACTION_DIM = 1
@@ -22,6 +27,11 @@ CRITIC_LR = 1e-6 * 5
 
 HIDDEN_LAYER_SIZE = 2048
 LAST_LAYER_SIZE = 32
+
+BATCH_SIZE = 1
+ENCODE_SPIKE_TRAINS = False
+
+GAMMA = 0.99
 
 
 class Actor(nn.Module):
@@ -63,9 +73,71 @@ class Critic(nn.Module):
         return self.network(state)
 
 
+def generate_state_tensor(observation, reward, env):
+    # adjust observation to the range [0, n]
+    if observation == "blank":
+        observation = -1
+    observation += 1
+
+    observation_one_hot = [0] * (env.number_of_stimuli + 1)
+    observation_one_hot[observation] = 1
+    observation_one_hot = torch.Tensor(observation_one_hot)
+    reward_one_hot = torch.Tensor([reward])
+    state_one_hot = torch.cat((observation_one_hot, reward_one_hot)).unsqueeze(0)
+    return state_one_hot
+
+
 def main():
     # Create the ChangeDetectionBasic environment
     env = ChangeDetectionBasic()
+
+    # Create the LPL Network
+    settings = Settings(
+        layer_sizes=[100, 100, 100, 100],  # Architecture layer sizes remain unchanged
+        data_size=env.number_of_stimuli + 2,  # extra for blank and reward
+        batch_size=BATCH_SIZE,
+        learning_rate=0.01,
+        epochs=10,
+        encode_spike_trains=ENCODE_SPIKE_TRAINS,
+        dt=.01,
+        percentage_inhibitory=50,
+        exc_to_inhib_conn_c=0.25,
+        exc_to_inhib_conn_sigma_squared=60,
+        layer_sparsity=0.9,
+        decay_beta=0.85,
+        tau_mean=1200,
+        tau_var=0.02,
+        tau_stdp=0.1,
+        tau_rise_alpha=0.005,
+        tau_fall_alpha=.05,
+        tau_rise_epsilon=0.002,
+        tau_fall_epsilon=0.02,
+        device=torch.device("cpu")
+    )
+
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="LPL-SNN-3",
+
+        # track hyperparameters and run metadata
+        config={
+            "architecture": "initial",
+            "dataset": "rl-change-detection",
+            "settings": settings,
+        }
+    )
+
+    net = Net(settings).to(device=settings.device)
+    visualizer = NetworkVisualizer(net)
+
+    actor_input_dim = sum(settings.layer_sizes)
+    actor = Actor(actor_input_dim, ACTION_DIM).to(device=settings.device)
+
+    critic_input_dim = sum(settings.layer_sizes)
+    critic = Critic(critic_input_dim).to(device=settings.device)
+
+    actor_optim = optim.Adam(actor.parameters(), lr=ACTOR_LR)
+    critic_optim = optim.Adam(critic.parameters(), lr=CRITIC_LR)
 
     # Set the number of episodes to run
     num_episodes = 5
@@ -78,12 +150,44 @@ def main():
         total_reward = 0
 
         # Run the episode until done
+        observation, reward, done, info = env.step(0)  # no lick to start (necessary because obs not provided on reset)
+        net.process_data_single_timestep(generate_state_tensor(observation, reward, env))
+        network_state = torch.cat(net.layer_activations(), dim=1)
         while not done:
             # Choose an action (0 for no lick, 1 for lick)
-            action = random.choice(env.action_space)  # Randomly select an action from the action_space list
+            probs = actor(network_state)
+            dist = torch.distributions.Categorical(probs)
+            action = dist.sample()
 
             # Take a step in the environment
             observation, reward, done, info = env.step(action)
+
+            # one hot encode observation and reward
+            state_one_hot = generate_state_tensor(observation, reward, env)
+
+            # feed into network and retrieve activations
+            net.process_data_single_timestep(state_one_hot)
+            next_network_state = torch.cat(net.layer_activations(), dim=1)
+
+            # feed into critic
+            next_value = critic(next_network_state)
+            value = critic(network_state)
+            td_error = reward + (GAMMA * next_value * (1 - int(done))) - value
+
+            # critic loss
+            critic_loss = td_error ** 2
+            critic_optim.zero_grad()
+            critic_loss.backward()
+            critic_optim.step()
+            wandb.log({"critic_loss": critic_loss})
+
+            # actor loss
+            actor_loss = -dist.log_prob(action) * td_error.detach()
+            prev_actor_loss = actor_loss.item()
+            actor_optim.zero_grad()
+            actor_loss.backward()
+            actor_optim.step()
+            wandb.log({"actor_loss": actor_loss})
 
             # Update the total reward for the episode
             total_reward += reward
@@ -92,8 +196,14 @@ def main():
             print(
                 f"Episode: {episode+1}, Time: {info['time']:.2f}, Action: {action}, Observation: {observation}, Reward: {reward}")
 
+            network_state = next_network_state
+
+            wandb.log({"total_reward": total_reward})
+
         # Print the episode summary
         print(f"Episode: {episode+1}, Total Reward: {total_reward}, Completed Trials: {info['completed_trials']}\n")
+
+        break
 
     # Close the environment
     env.close()
